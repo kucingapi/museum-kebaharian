6a27b144550e21df2ef1fc02c9cbc74ffde34f07
noveel bafagih <novel.bafagih@udana.id>
2022-11-04 17:55:20 +0700

    [ADD] all dependencies

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-head-element */
+
+import '../styles/globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head></head>
+      <body>{children}</body>
+    </html>
+  );
+}
