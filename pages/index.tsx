56dcb17e7d698239d7eca86fd82f0d56f994b9c9
noveel bafagih <novel.bafagih@udana.id>
2022-11-06 23:46:19 +0700

    [ADD] 404 not found

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
-import { AnimatePresence,motion } from "framer-motion";
+import { AnimatePresence, motion } from 'framer-motion';
+import logo from '../public/404-error-rafiki.svg';
 
 export default function Home() {
+  const message = 'Oops Something Went Wrong'.split('');
   return (
-      <motion.div
-        animate={{
-          x: 0,
-          y: 0,
-          scale: 1,
-          rotate: 0,
-        }}
-      >
-      <button className="btn">Hello daisyUI</button>
-
-      </motion.div>
+    <>
+      <div className="w-full flex items-center flex-col p-5">
+        <img className="w-1/2" src={logo.src} alt="404 not found" />
+        <article className="prose flex gap-2 align-baseline max-w-max">
+          {message.map((value) => {
+            return (
+              <motion.h1
+                whileHover={{
+                  scale: 1.7,
+                  color: ['#C7DBFF', '#62B6CB', '#FFE999'],
+                  transition: { type: 'spring', mass: 1 },
+                }}>
+               
+                {value}
+              </motion.h1>
+            );
+          })}
+        </article>
+      </div>
+    </>
   );
 }
