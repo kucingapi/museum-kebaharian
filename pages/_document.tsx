6b80c5b19a4c51b9ecef8e88fd71f05bebb63e15
noveel bafagih <novel.bafagih@udana.id>
2022-11-09 10:33:42 +0700

    [ADD] google verifier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <meta
+            name="google-site-verification"
+            content="PWe5eOUOK6HT4gZ0beecGFUrDMdL7FYtlfgqE0M5eFI"
+          />
         </Head>
         <body>
           <Main />
