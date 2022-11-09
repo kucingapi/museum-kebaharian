882682256ae4b667baf1f01bf7553816173a6652
noveel bafagih <novel.bafagih@udana.id>
2022-11-09 10:25:37 +0700

    [ADD] analytics for vercell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-head-element */
 import { Roboto } from '@next/font/google';
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 
 const roboto = Roboto({ weight: ['100', '300', '400', '500', '700', '900'] });
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={roboto.className}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
